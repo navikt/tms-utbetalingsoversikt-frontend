748881fd5339bc2abfa5bb0e6ef3f9f153a98f5d
nima-hakimi <74723240+nima-hakimi@users.noreply.github.com>
2024-10-25 15:32:32 +0200

    Use @navikt/oasis instead of manual validation (#17)
    Use @navikt/oasis instead of manually validating the token

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,35 +1,39 @@
-import { validateIdportenToken } from "./auth/validate";
 import { defineMiddleware } from "astro/middleware";
 import { loginUrl } from "./urls";
 import { isInternal } from "./utils";
 import { isLocal } from "@src/utils/server/urls.ts";
+import { getToken, validateToken } from "@navikt/oasis";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  const bearerToken: string | null | undefined = context.request.headers.get("authorization");
+  const token = getToken(context.request.headers);
   const params = encodeURIComponent(context.url.search);
 
   if (isLocal) {
     return next();
   }
-  
+
   if (isInternal(context)) {
     return next();
   }
 
-  if (!bearerToken) {
-    console.info("Could not find any bearer token on the request. Redirecting to login.");
+  if (!token) {
+    console.info(
+      "Could not find any bearer token on the request. Redirecting to login."
+    );
     return context.redirect(`${loginUrl}${params}`);
   }
 
-  const validationResult = await validateIdportenToken(bearerToken);
+  const validation = await validateToken(token);
 
-  if (validationResult !== "valid") {
-    const error = new Error(`Invalid JWT token found (cause: ${validationResult.errorType} ${validationResult.message}, redirecting to login.`);
+  if (!validation.ok) {
+    const error = new Error(
+      `Invalid JWT token found (cause: ${validation.errorType} ${validation.error}, redirecting to login.`
+    );
     console.error(error);
     return context.redirect(`${loginUrl}${params}`);
   }
 
-  context.locals.token = bearerToken;
+  context.locals.token = token;
 
   return next();
 });
