9a95cfcd39583d0e310ec08a1e5ad956a014fde5
Nima Hakimi <nima.hakimi2@nav.no>
2024-10-29 12:49:29 +0100

    Add faro

---
+++ src/hooks/useFaro.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { initializeFaro } from "@grafana/faro-web-sdk";
+import { telemetryUrl } from "@src/utils/client/urls.ts";
+
+const telemetryConfig = {
+  telemetryCollectorURL: telemetryUrl,
+  app: {
+    name: "tms-min-side",
+  },
+};
+
+export const useFaro = () => {
+  useEffect(() => {
+    initializeFaro({
+      url: telemetryConfig.telemetryCollectorURL,
+      app: telemetryConfig.app,
+    });
+  }, []);
+};
