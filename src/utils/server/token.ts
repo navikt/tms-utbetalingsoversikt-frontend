c3f7f6a5b71c36cfcc4eb1095f1cd59d93bd42f5
Nima Hakimi <nima.hakimi2@nav.no>
2024-11-04 13:06:20 +0100

    Talk to PDL

--- src/utils/server/token.ts
+++ src/utils/server/token.ts
@@ -1,7 +1,7 @@
 import { requestOboToken } from '@navikt/oasis';
 import { isLocal } from '@src/utils/server/environment.ts';
 
-const audience = `${process.env.NAIS_CLUSTER_NAME}:tms-utbetalingsoversikt-frontend`;
+const audience = 'dev-fss:pdl:pdl-api';
 
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
