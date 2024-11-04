e7a76ac9d13ece362b47252bcccb02da24342981
Nima Hakimi <nima.hakimi2@nav.no>
2024-11-04 12:37:16 +0100

    Correct getOboToken

--- src/utils/server/token.ts
+++ src/utils/server/token.ts
@@ -1,7 +1,7 @@
 import { requestOboToken } from '@navikt/oasis';
 import { isLocal } from '@src/utils/server/environment.ts';
 
-const audience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-utkast`;
+const audience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-utbetalingsoversikt-frontend`;
 
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
@@ -12,7 +12,9 @@ export const getOboToken = async (token: string): Promise<string> => {
 
   if (!oboResult.ok) {
     console.error('Error getting access token: ' + oboResult.error);
-    throw new Error('Request oboToken for tms-utkast failed ');
+    throw new Error(
+      'Request oboToken for tms-utbetalingsoversikt-frontend failed ',
+    );
   }
 
   return oboResult.token;
