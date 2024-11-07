1d4c0260f433c57cfd25da921a591d44831b68c5
Nima Hakimi <nima.hakimi2@nav.no>
2024-11-07 09:52:01 +0100

    Set token to be an empty string when local

--- src/pages/api/navn.ts
+++ src/pages/api/navn.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from 'astro';
 import { parseIdportenToken } from '@navikt/oasis';
 import { getOboToken } from '@src/utils/server/token';
 import { fetchNavn } from '@src/utils/server/api/fetchNavn';
+import { isLocal } from '@src/utils/server/urls';
 
 export const GET: APIRoute = async (context) => {
   const parsedToken = parseIdportenToken(context.locals.token);
@@ -11,7 +12,7 @@ export const GET: APIRoute = async (context) => {
     return new Response(null, { status: 500 });
   }
   const pid = parsedToken.pid;
-  const token = await getOboToken(context.locals.token);
+  const token = isLocal ? '' : await getOboToken(context.locals.token);
   const fetchedData = await fetchNavn(token, pid);
 
   return new Response(JSON.stringify(fetchedData), {
