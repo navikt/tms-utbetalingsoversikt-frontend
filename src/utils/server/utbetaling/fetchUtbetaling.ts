af8585404f1c989f27ff04ab0e02ea26ca569b97
nima-hakimi <74723240+nima-hakimi@users.noreply.github.com>
2024-12-20 11:57:47 +0100

    Format fetchUtbetaling (#69)
    Move enkelUtbetaling fetch function out of utbetalingPage. try&catch in utbetalingPage

---
+++ src/utils/server/utbetaling/fetchUtbetaling.ts
@@ -0,0 +1,29 @@
+import { enkelUtbetalingApiUrl } from '@src/utils/server/urls';
+import type { Utbetaling } from '@src/types/types';
+import { getOboToken } from '@src/utils/server/token';
+
+export const fetchUtbetaling = async (
+  token: string,
+  id: string,
+): Promise<Utbetaling> => {
+  const utbetalingsApiAudience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-utbetalingsoversikt-api`;
+
+  const oboToken = await getOboToken(token, utbetalingsApiAudience);
+
+  const response = await fetch(enkelUtbetalingApiUrl(id), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${oboToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      'http error with status ' + response.status + 'when fetching utbetaling',
+    );
+  }
+
+  return await response.json();
+};
