931721c996a751f202341a353938b7afd5f2663e
Nima Hakimi <74723240+nima-hakimi@users.noreply.github.com>
2025-01-17 15:32:50 +0100

    amplitude print btn (#74)
    * Initialize amplitude in layout
    
    * Add amplitude logEvent to print button

--- src/components/prinButton/PrintButton.tsx
+++ src/components/prinButton/PrintButton.tsx
@@ -1,12 +1,16 @@
 import { PrinterSmallIcon } from '@navikt/aksel-icons';
 import { Button } from '@navikt/ds-react';
 import styles from './PrintButton.module.css';
+import { logEvent } from '@src/utils/client/amplitude';
 
 const PrintButton = () => {
   return (
     <Button
       className={styles.skrivUtButton}
-      onClick={() => window.print()}
+      onClick={() => {
+        window.print();
+        logEvent('skriv-ut-utbetaling', 'skriv-ut');
+      }}
       icon={<PrinterSmallIcon aria-hidden />}
     >
       Skriv ut
