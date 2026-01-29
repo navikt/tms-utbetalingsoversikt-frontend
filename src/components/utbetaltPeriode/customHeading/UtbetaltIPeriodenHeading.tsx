0b707c2d64cb87abad77e1738e8772a8689fea4d
chris-santa <57258371+chris-santa@users.noreply.github.com>
2026-01-29 14:41:52 +0100

    A11y endringer (#103)
    Diverse endringer for å tilpasse tilgjengelighetskrav.

---
+++ src/components/utbetaltPeriode/customHeading/UtbetaltIPeriodenHeading.tsx
@@ -0,0 +1,27 @@
+import { Detail, Heading } from "@navikt/ds-react";
+import style from "@src/components/utbetaltPeriode/UtbetaltPeriode.module.css";
+
+type Props = {
+  isPrint?: boolean;
+  periode: string;
+};
+
+const UtbetaltIPeriodenHeading = ({ isPrint, periode }: Props) => {
+  if (isPrint) {
+    return (
+      <>
+        <Detail weight="semibold">Utbetalt i perioden</Detail>
+        <Detail weight="semibold">{periode}</Detail>
+      </>
+    );
+  } else {
+    return (
+      <Heading className={`navds-body-short`} level="2" size="small">
+        <span>Utbetalt i perioden</span>
+        <span className={style.periodeDate}>{periode}</span>
+      </Heading>
+    );
+  }
+};
+
+export default UtbetaltIPeriodenHeading;
